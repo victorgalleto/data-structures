180d4d8c0b505019fbdaf303bc4c2a05740aee6a
Galleto <victorbgalleto@gmail.com>
2023-02-19 18:50:41 -0300

    inserting and removing elements in a linked list

--- linked_list.py
+++ linked_list.py
@@ -36,11 +36,61 @@ class LinkedList:
 
         itr.next = Node(data, None)   
 
-    
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        
+        return count
+
+    def remove_at(self, index):
+        if index<0 or index >= self.get_length():
+            raise Exception("invalid index")
+
+        if index==0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index<0 or index >= self.get_length():
+            raise Exception("invalid index")
+
+        if index==0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert_at_begining(5)
-    ll.insert_at_begining(89)
+    ll.insert_values(['banana', 'mango', 'grapes', 'orange'])
+    ll.print()
+    ll.insert_at(0,'figs')
+    ll.insert_at(2,'jackfruit')
     ll.print()
 
