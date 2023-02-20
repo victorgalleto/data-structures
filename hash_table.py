d9c06ee9e1f93a56732be7533f128d5bb3868d7e
Galleto <victorbgalleto@gmail.com>
2023-02-20 00:34:12 -0300

    SImple hash table

---
+++ hash_table.py
@@ -0,0 +1,30 @@
+stock_prices = []
+
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+    
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+t = HashTable()
+t['march 6'] = 130
+t['march 1'] = 20
+t['dec 17'] = 27
+print(t.get_hash('march 1'))
