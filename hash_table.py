8c5a6b52bc42716df3bcceeda894bcd5c04d3511
Galleto <victorbgalleto@gmail.com>
2023-02-20 01:56:33 -0300

    Dealing with collisions

--- hash_table.py
+++ hash_table.py
@@ -3,28 +3,45 @@ stock_prices = []
 class HashTable:
     def __init__(self):
         self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.arr = [[] for i in range(self.MAX)]
 
     def get_hash(self, key):
         h = 0
         for char in key:
             h += ord(char)
         return h % self.MAX
-
-    def __setitem__(self, key, val):
-        h = self.get_hash(key)
-        self.arr[h] = val
     
     def __getitem__(self, key):
         h = self.get_hash(key)
-        return self.arr[h]
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        found = False
+        for idx, element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][idx] = (key, val)
+                found = True
+                break
+        if not found:
+            self.arr[h].append((key, val))
+
 
     def __delitem__(self, key):
         h = self.get_hash(key)
-        self.arr[h] = None
+        for index, element in enumerate(self.arr[h]):
+            if element[0] == key:
+                del self.arr[h][index]
+            
 
 t = HashTable()
-t['march 6'] = 130
+t['march 6'] = 120
 t['march 1'] = 20
 t['dec 17'] = 27
-print(t.get_hash('march 1'))
+t['march 17'] = 459 # Collision with 'march 6'
+
+#del t['march 17']
+
+print(t.arr)
