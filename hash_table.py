d8e2467c00799eef7c0cc63ec703d40389a4050b
Galleto <victorbgalleto@gmail.com>
2023-02-21 16:15:32 -0300

    Hash table handling csv

--- hash_table.py
+++ hash_table.py
@@ -3,12 +3,14 @@ stock_prices = []
 class HashTable:
     def __init__(self):
         self.MAX = 100
+        self.re = []
         self.arr = [[] for i in range(self.MAX)]
+        
 
     def get_hash(self, key):
         h = 0
         for char in key:
-            h += ord(char)
+            h += ord(char) # ASCII value
         return h % self.MAX
     
     def __getitem__(self, key):
@@ -37,11 +39,9 @@ class HashTable:
             
 
 t = HashTable()
-t['march 6'] = 120
+t['march 6'] = 50
 t['march 1'] = 20
 t['dec 17'] = 27
 t['march 17'] = 459 # Collision with 'march 6'
-
-#del t['march 17']
 
 print(t.arr)
--- question_1.py
+++ question_1.py
@@ -1,13 +1,50 @@
 import csv
 
-class ReadOutput:
+class HashTable:
+
+    def __init__(self):
+        self.size = 100
+        self.temps = [[] for i in range(self.size)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.size
+
+    def __setitem__(self, key, value):
+        h = self.get_hash(key)
+        self.temps[h] += [[key, value]]
+    
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        for element in self.temps[h]:
+            if element[0] == key:
+                return element[1]
 
     def readCsv(self):      
         with open('nyc_weather.csv', 'r') as f:
-            csvReader = csv.reader(f)
-            days = []
-            for row in csvReader:
-                days.append((row))
-        
+            reader = csv.DictReader(f)
+            items = list(reader)
+            for item in items:
+                self.__setitem__(item['date'], int(item['temperature(F)']))
     
+    def averaging(self, start, end):
+        start_day = int(start.split(' ')[1])
+        end_day = int(end.split(' ')[1])
+        sum = 0
+        
+        for day in range(end_day - start_day + 1):
+            temp = self.__getitem__('Jan ' + str(day + 1))
+            sum += int(temp)
+        return sum / (end_day - start_day + 1)
+        
+
+output = HashTable()
+
+output.readCsv()
+
+print(output.averaging('Jan 1', 'Jan 7'))
+
+
 
